4337a86f96d59e4c018414bb104e05d0cb039738
MasonMarc <masonmarcantel@gmail.com>
2023-01-10 18:28:23 -0500

    added functionality for adding a role and employee but without correct ids

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const init = () => {
         type: 'rawlist',
         name: 'selection',
         message: 'choose one of the following',
-        choices: ['VIEW ALL EMPLOYEES', 'VIEW ALL DEPARTMENTS', 'VIEW ALL ROLES', 'ADD A DEPARTMENT']
+        choices: ['VIEW ALL EMPLOYEES', 'VIEW ALL DEPARTMENTS', 'VIEW ALL ROLES', 'ADD A DEPARTMENT', 'ADD A ROLE', 'ADD AN EMPLOYEE']
     })
         .then((answers) => {
 
@@ -50,12 +50,61 @@ const chooseOption = (selection) => {
                 message: 'please enter new department name'
             })
                 .then((answers) => {
-                    console.log(answers.name);
                     db.query(`INSERT INTO department (name)
         VALUES
         ('${answers.name}');`,
                         (err, departments) => {
                             console.table(departments)
+                            init();
+                        });
+                })
+            break;
+        }
+        case 'ADD A ROLE': {
+            prompt([{
+                type: 'input',
+                name: 'title',
+                message: 'please enter name of new role'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'please enter salary of new role'
+            },
+            ])
+                .then((answers) => {
+                    console.log(answers.title);
+                    console.log(answers.salary);
+                    db.query(`INSERT INTO role (title, salary, department_id)
+                    VALUES
+                    ('${answers.title}', ${answers.salary}, 1);`,
+                        (err, roles) => {
+                            console.table(roles)
+                            init();
+                        });
+                })
+            break;
+        }
+        case 'ADD AN EMPLOYEE': {
+            prompt([{
+                type: 'input',
+                name: 'first_name',
+                message: 'please enter first name of new employee'
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'please enter last name of new employee'
+            },
+            ])
+                .then((answers) => {
+                    console.log(answers.first_name);
+                    console.log(answers.last_name);
+                    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                    VALUES
+                    ('${answers.first_name}', '${answers.last_name}', 1, 1);`,
+                        (err, employees) => {
+                            console.table(employees)
                             init();
                         });
                 })
