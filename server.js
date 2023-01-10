a657ecb80dd242da9e83e7135b951f24ee4d3c21
MasonMarc <masonmarcantel@gmail.com>
2023-01-10 18:16:33 -0500

    added ability to add a department to the database

--- server.js
+++ server.js
@@ -7,19 +7,6 @@ const db = mysql.createConnection({
     database: "employee_db",
 })
 
-// let chooseList = () => {
-//     const prompt = inquirer.createPromptModule();
-//     prompt({
-//         type: 'list',
-//         name: 'selection',
-//         message: 'choose one of the following',
-//         choices: ['VIEW ALL EMPLOYEES', 'VIEW ALL DEPARTMENTS', 'VIEW ALL ROLES', 'ADD A DEPARTMENT']
-//     })
-//         .then((answers) => {
-
-//             chooseOption(answers);
-//         })
-// };
 const init = () => {
     prompt({
         type: 'rawlist',
@@ -35,28 +22,46 @@ const init = () => {
 
 const chooseOption = (selection) => {
     switch (selection) {
-    case 'VIEW ALL EMPLOYEES': {
-        db.query('SELECT * FROM employee', (err, employees) => {
-            console.table(employees)
-            init();
-        });
-        break;
+        case 'VIEW ALL EMPLOYEES': {
+            db.query('SELECT * FROM employee', (err, employees) => {
+                console.table(employees)
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL DEPARTMENTS': {
+            db.query('SELECT * FROM department', (err, departments) => {
+                console.table(departments)
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL ROLES': {
+            db.query('SELECT * FROM role', (err, roles) => {
+                console.table(roles)
+                init();
+            });
+            break;
+        }
+        case 'ADD A DEPARTMENT': {
+            prompt({
+                type: 'input',
+                name: 'name',
+                message: 'please enter new department name'
+            })
+                .then((answers) => {
+                    console.log(answers.name);
+                    db.query(`INSERT INTO department (name)
+        VALUES
+        ('${answers.name}');`,
+                        (err, departments) => {
+                            console.table(departments)
+                            init();
+                        });
+                })
+            break;
+        }
     }
-    case 'VIEW ALL DEPARTMENTS': {
-        db.query('SELECT * FROM department', (err, departments) => {
-            console.table(departments)
-            init();
-        });
-        break;
-    }
-    case 'VIEW ALL ROLES': {
-        db.query('SELECT * FROM role', (err, roles) => {
-            console.table(roles)
-            init();
-        });
-        break;
-    }
-}
 };
 
 
